d10de49242570e4de4cab9695c0cefaa1f5d20bf
Tirumalareddy jaswanth kumar reddy <155978069+jaswanthkumarreddy999@users.noreply.github.com>
2025-05-26 13:26:40 +0530

    Add files via upload

---
+++ striver/pattern/pattern7.java
@@ -0,0 +1,19 @@
+package pattern;
+
+import java.util.Scanner;
+
+public class pattern7{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++) {
+            for(int j=n-i;j>=1;j--) {
+                System.out.print(" ");
+            }
+            for(int k=(2*i)-1;k>=1;k--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
