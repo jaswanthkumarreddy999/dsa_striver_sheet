4d3bd3b4911b47c4d9b35d3cf0fcec91b26c61b2
T.Jaswanth kumar reddy <tjaswanthkumarreddy@example.com>
2025-07-02 19:15:00 +0530

    completed

--- striver/S3L1/lsum.java
+++ striver/S3L1/lsum.java
@@ -1,37 +1,40 @@
 package S3L1;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class lsum {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        int[] nums = new int[size];
-        for(int i=0;i<size;i++){
-            nums[i]=sc.nextInt();
-        }
-        int rsum = sc.nextInt();
-        System.out.println(lsa(nums,rsum));
+    public static void main(String[] args) {
+        int[] nums = {2,3,5,1,9};
+        int k = 10;
+        System.out.println(lsa2(nums,k));  
     }
-    public static int lsa(int[] nums,int rsum){
-        int lp = 0;
-        int rp = 0;
-        int sum = 0;
-        while(rp<nums.length && lp<nums.length){
-            if(sum<rsum){
-                sum+=nums[rp];
-                System.out.println(lp+" "+rp+" "+sum);
-                rp++;
-            } else if (sum==rsum) {
-                System.out.println("Found the answer "+(rp-lp));
-                sum+=nums[rp];
-                rp++;
-            } else{
-                sum-=nums[lp];
-                lp++;
+    public static int  lsa1(int[] nums,int k){
+        int ml=0;
+        for(int i=0;i<nums.length;i++){
+            int sum =0;
+            for(int j=i;j<nums.length;j++){
+                sum+=nums[j];
+                if(sum==k){
+                    ml=Math.max(ml,j-i+1);
+                }
             }
-
         }
-        return 0;
+        return ml;
     }
+    public static int  lsa2(int[] nums,int k){
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        hm.put(0,-1);
+        int ml=0;
+        int sum = 0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            hm.put(sum,i);
+            if(hm.containsKey(sum-k)){
+                ml = Math.max(i-hm.get(sum-k),ml);
+            }     
+        }
+        return ml;
+    }
+   
 }
---
+++ striver/S3L1/lsum.py
@@ -0,0 +1,25 @@
+nums = [2,3,5,1,9]
+k = 10
+
+def lsa1(nums,k):
+    ml = 0
+    for i in range(0,len(nums)):
+        sum=0
+        for j in range(i,len(nums)):
+            sum+=nums[j]
+            if sum == k:
+                ml = max(ml,j-i+1)
+    return ml
+
+def lsa2(nums,k):
+    hs = {}
+    hs[0]=-1
+    sum=0
+    ml=0
+    for i in range(0,len(nums)):
+        sum+=nums[i]
+        hs[sum]=i
+        if sum-k in hs:
+            ml=max(i-hs[sum-k],ml)
+    return ml
+print(lsa2(nums,k))
